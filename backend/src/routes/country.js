53b40e88171d6ef4194906439ef9dace4cede0b2
Ivann <ivann.machado@laplateforme.io>
2026-01-29 10:38:33 +0100

    deleted test files

--- backend/src/routes/country.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const countryCtrl = require("../controllers/country");
-
-router.get("/", countryCtrl.getCountry);
-
-module.exports = router;
