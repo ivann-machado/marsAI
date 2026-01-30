3187cebbf35b7fcf0758fe8fb4906b2a327099ff
pierre-canet <pierre.canet@laplateforme.io>
2026-01-30 16:55:39 +0100

    v0.6 ajout de la traduction i18n

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import viteLogo from '/vite.svg'
 import reactLogo from '../../assets/react.svg'
+import { useTranslation } from 'react-i18next';
 
 function Header() {
+    const { t } = useTranslation();
     const [isOpen, setIsOpen] = useState(false);
     const toggleMenu = () => {
         setIsOpen(!isOpen)
@@ -26,9 +28,11 @@ function Header() {
         }
     }, [isOpen])
     return (
-        <header className="w-screen p-4 border-red-600 border">           
-            <nav className="border-2 border-black/25 rounded-full bg-black/5 max-w-1200px mx-auto p-4  flex flex-row justify-between items-center ">
-                <p className="text-fuchsia-800"><strong>MarsAI</strong></p>
+        <header className="w-screen p-4 border-red-600 border bg-gray-900">           
+            <nav className="border-2 border-black/25 rounded-full bg-white/30 max-w-1200px mx-auto p-6  flex flex-row justify-between items-center ">
+            <div className="flex flex-row justify-center items-center border-red-600 border">
+                <p className="text-2xl font-bold text-white hover:cursor-pointer">MARS <span className="text-4xl text-indigo-500 font-bold">AI</span></p>
+            </div>                
                 {!isOpen && (
                         <div onClick={toggleMenu} className="cursor-pointer md:hidden">
                             <img src={viteLogo} className="logo" alt="Vite logo" size={30} />
@@ -44,13 +48,13 @@ function Header() {
                             <ul className="bg-gray-800 overflow-y-hidden fixed z-10 top-0 left-0
                             w-screen min-h-screen flex justify-center items-center flex-col gap-10
                             duration-300 ease-in border-red-600 border" >
-                                <li>Accueil</li>
-                                <li>Films</li>
-                                <li>Participer</li>
-                                <li>Jury</li>
-                                <li>Partenaires</li>
-                                <li>A propos</li>
-                                <li>Evènements</li>
+                                <li className="hover:underline">{t('home')}</li>
+                                <li className="hover:underline">{t('gallery')}</li>
+                                <li className="hover:underline">{t('participate')}</li>
+                                <li className="hover:underline">{t('board')}</li>
+                                <li className="hover:underline">{t('partners')}</li>
+                                <li className="hover:underline">{t('about')}</li>
+                                <li className="hover:underline">{t('schedule')}</li>
                             </ul>
                         </div>
                     ):(
@@ -59,13 +63,13 @@ function Header() {
                             duration-300 ease-in"></div>                        
                     )}
                     <ul className="items-center gap-4 hidden md:flex" >
-                        <li>Accueil</li>
-                        <li>Films</li>
-                        <li>Participer</li>
-                        <li>Jury</li>
-                        <li>Partenaires</li>
-                        <li>A propos</li>
-                        <li>Evènements</li>
+                        <li className="hover:underline hover:cursor-pointer">{t('home')}</li>
+                        <li className="hover:underline hover:cursor-pointer">{t('gallery')}</li>
+                        <li className="hover:underline hover:cursor-pointer">{t('participate')}</li>
+                        <li className="hover:underline hover:cursor-pointer">{t('board')}</li>
+                        <li className="hover:underline hover:cursor-pointer">{t('partners')}</li>
+                        <li className="hover:underline hover:cursor-pointer">{t('about')}</li>
+                        <li className="hover:underline hover:cursor-pointer">{t('schedule')}</li>
                     </ul>              
                 </nav>
         </header>
