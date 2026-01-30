a781b4e94a1788d6a63c9b54f66f5567305b6257
pierre-canet <pierre.canet@laplateforme.io>
2026-01-30 16:55:39 +0100

    v0.6 ajout de la traduction i18n

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,21 +1,77 @@
-function Header(){
-    return(
-        <header>
-            <nav className="flex flex-row justify-around align-center gap-5  bg-green-200 border-2 border-green-600 p-2 w-screen">
-                <div className="flex flex-row justify-evenly grow-2 border-2 border-black p-3 rounded-4xl bg-white opacity-5">
-                    <img className="border-2 border-black p-2" alt="Insert a logo here" />
-                    <ul className="flex flex-row justify-around grow-2 border-2 border-black p-2 rounded-3xl *:opacity-100" >
-                    <li className="border border-red-600">Accueil</li>
-                    <li className="border border-red-600">Films</li>
-                    <li className="border border-red-600">Participer</li>
-                    <li className="border border-red-600">Jury</li>
-                    <li className="border border-red-600">Partenaires</li>
-                    <li className="border border-red-600">A propos</li>
-                    <li className="border border-red-600">Evènements</li>
-                </ul>
-                </div>
-                
-            </nav>
+import { useEffect, useState } from "react";
+import viteLogo from '/vite.svg'
+import reactLogo from '../../assets/react.svg'
+import { useTranslation } from 'react-i18next';
+
+function Header() {
+    const { t } = useTranslation();
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    };
+    useEffect(() =>{
+        const handleResize = () =>{
+            if (window.innerWidth >= 768){
+                setIsOpen(false)
+            }
+        }
+        window.addEventListener('resize', handleResize)
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    })
+    useEffect(() =>{
+        if (isOpen){
+            document.body.classList.add('no-scroll');
+        } else {
+            document.body.classList.remove('no-scroll');
+        }
+    }, [isOpen])
+    return (
+        <header className="w-screen p-4 border-red-600 border bg-gray-900">           
+            <nav className="border-2 border-black/25 rounded-full bg-white/30 max-w-1200px mx-auto p-6  flex flex-row justify-between items-center ">
+            <div className="flex flex-row justify-center items-center border-red-600 border">
+                <p className="text-2xl font-bold text-white hover:cursor-pointer">MARS <span className="text-4xl text-indigo-500 font-bold">AI</span></p>
+            </div>                
+                {!isOpen && (
+                        <div onClick={toggleMenu} className="cursor-pointer md:hidden">
+                            <img src={viteLogo} className="logo" alt="Vite logo" size={30} />
+                        </div>
+                    )}
+                    {isOpen && (
+                        <div onClick={toggleMenu} className="cursor-pointer md:hidden z-20 fixed top-6 right-6">
+                            <img src={reactLogo} className="logo react" alt="React logo" size={30} />
+                        </div>
+                    )}
+                    {isOpen ? (
+                        <div>
+                            <ul className="bg-gray-800 overflow-y-hidden fixed z-10 top-0 left-0
+                            w-screen min-h-screen flex justify-center items-center flex-col gap-10
+                            duration-300 ease-in border-red-600 border" >
+                                <li className="hover:underline">{t('home')}</li>
+                                <li className="hover:underline">{t('gallery')}</li>
+                                <li className="hover:underline">{t('participate')}</li>
+                                <li className="hover:underline">{t('board')}</li>
+                                <li className="hover:underline">{t('partners')}</li>
+                                <li className="hover:underline">{t('about')}</li>
+                                <li className="hover:underline">{t('schedule')}</li>
+                            </ul>
+                        </div>
+                    ):(
+                        <div className="bg-gray-800 overflow-y-hidden fixed z-10 top-0 left-[-150%]
+                            w-screen min-h-screen flex justify-center items-center flex-col gap-10
+                            duration-300 ease-in"></div>                        
+                    )}
+                    <ul className="items-center gap-4 hidden md:flex" >
+                        <li className="hover:underline hover:cursor-pointer">{t('home')}</li>
+                        <li className="hover:underline hover:cursor-pointer">{t('gallery')}</li>
+                        <li className="hover:underline hover:cursor-pointer">{t('participate')}</li>
+                        <li className="hover:underline hover:cursor-pointer">{t('board')}</li>
+                        <li className="hover:underline hover:cursor-pointer">{t('partners')}</li>
+                        <li className="hover:underline hover:cursor-pointer">{t('about')}</li>
+                        <li className="hover:underline hover:cursor-pointer">{t('schedule')}</li>
+                    </ul>              
+                </nav>
         </header>
     )
 }
