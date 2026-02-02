bd6bb00457909f735af5e81f53b4692395145be9
pierre-canet <pierre.canet@laplateforme.io>
2026-02-02 14:45:12 +0100

    v0.3 ajout de la version basique du formulaire

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Homepage from './pages/Homepage'
+import UploadPage from './pages/Upload_page'
 
 function App() {
  
   return (
     <>
-    <Homepage />
+    <UploadPage />
     </>
   )
 }
--- frontend/src/components/Upload_Form/Upload_Form.jsx
+++ frontend/src/components/Upload_Form/Upload_Form.jsx
@@ -3,6 +3,56 @@ import viteLogo from '/vite.svg'
 import reactLogo from '../../assets/react.svg'
 import { useTranslation } from 'react-i18next';
 
-function UploadForm(){}
+function UploadForm(){
+    return(
+        <form>
+            <div>
+                <label htmlFor="">Titre</label>
+                <input type="text" name="" id="" className="border border-black rounded-lg" />
+            </div>
+            <div>
+                <label htmlFor="">Durée</label>
+                <input type="text" name="" id="" className="border border-black rounded-lg" />
+            </div>
+            <div>
+                <label htmlFor="">Image</label>
+                <input type="file" name="" id="" className="border border-black rounded-lg" />
+            </div>
+            <div>
+                <label htmlFor="">IA scénario</label>
+                <input type="text" name="" id="" className="border border-black rounded-lg" />
+            </div>
+            <div>
+                <label htmlFor="">IA générative vidéo</label>
+                <input type="text" name="" id="" className="border border-black rounded-lg" />
+            </div>
+            <div>
+                <label htmlFor="">IA sons et musiques</label>
+                <input type="text" name="" id="" className="border border-black rounded-lg" />
+            </div>
+            <div>
+                <label htmlFor="">IA post-production</label>
+                <input type="text" name="" id="" className="border border-black rounded-lg" />
+            </div>
+            <div>
+                <label htmlFor="">Producteur</label>
+                <input type="text" name="" id="" className="border border-black rounded-lg" />
+            </div>
+            <div>
+                <label htmlFor="">Instagram</label>
+                <input type="text" name="" id="" className="border border-black rounded-lg" />
+            </div>
+            <div>
+                <label htmlFor="">Linkedin</label>
+                <input type="text" name="" id="" className="border border-black rounded-lg" />
+            </div>
+            <div>
+                <label htmlFor="">Youtube</label>
+                <input type="text" name="" id="" className="border border-black rounded-lg" />
+            </div>
+            <button type="submit">Valider</button>
+        </form>
+    )
+}
 
 export default UploadForm;
