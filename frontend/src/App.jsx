c1842e50cde2eebf6d507d98eb0362f64a1a8a61
cosmin-bilga <cosmin.bilga@laplateforme.io>
2026-02-02 15:47:47 +0100

    VideoDetail v.09

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,18 @@
-import './App.css'
-import Homepage from './pages/Homepage'
-import VideoDetail from './pages/Video_detail'
-import Footer from './components/Footer/Footer';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import "./App.css";
+import Homepage from "./pages/Homepage";
+import VideoDetail from "./pages/VideoDetail";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
- 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/video/:videoId" element={<VideoDetail />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/video/:videoId" element={<VideoDetail />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/AIList/AIList.jsx
+++ frontend/src/components/AIList/AIList.jsx
@@ -4,11 +4,15 @@ const AIList = ({ type, data }) => {
   const items = data.split(",").map((item) => item.trim());
 
   return (
-    <div>
-      <p>{type}</p>
-      {items.map((item, index) => (
-        <p key={index}>{item}</p>
-      ))}
+    <div className="m-4">
+      <p className="text-gray-200">{type}</p>
+      <div className="flex gap-4 flex-wrap">
+        {items.map((item, index) => (
+          <p className="border rounded-xl bg-gray-800 p-2" key={index}>
+            {item}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
