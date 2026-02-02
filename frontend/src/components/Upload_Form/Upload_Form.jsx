609c14f59220d4da743d6727c5cf93c445fcdc46
pierre-canet <pierre.canet@laplateforme.io>
2026-02-02 10:54:48 +0100

    v0.2 initialisation de la page UploadPage

---
+++ frontend/src/components/Upload_Form/Upload_Form.jsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import viteLogo from '/vite.svg'
+import reactLogo from '../../assets/react.svg'
+import { useTranslation } from 'react-i18next';
+
+function UploadForm(){}
+
+export default UploadForm;
