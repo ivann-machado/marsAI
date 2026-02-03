6055ebc8f8bb4716addaf997490b816232c991fc
pierre-canet <pierre.canet@laplateforme.io>
2026-02-03 09:46:44 +0100

    v0.3.5 changements mineurs au CSS

--- frontend/src/components/Upload_Form/Upload_Form.jsx
+++ frontend/src/components/Upload_Form/Upload_Form.jsx
@@ -5,52 +5,66 @@ import { useTranslation } from 'react-i18next';
 
 function UploadForm(){
     return(
-        <form>
-            <div>
-                <label htmlFor="">Titre</label>
-                <input type="text" name="" id="" className="border border-black rounded-lg" />
+        <form className="bg-gray-800">
+            <h3>Postez votre film !</h3>
+            <div className="border border-black">
+                <p>Informations globales</p>
+                <div>
+                    <label htmlFor="">Titre</label>
+                    <input type="text" name="" id="" className="border border-black rounded-lg" />
+                </div>
+                <div>
+                    <label htmlFor="">Durée</label>
+                    <input type="text" name="" id="" className="border border-black rounded-lg" />
+                </div>
+                <div>
+                    <label htmlFor="">Vidéo</label>
+                    <input type="file" name="" id="" className="border border-black rounded-lg" />
+                </div>
+                <div>
+                    <label htmlFor="">Image</label>
+                    <input type="file" name="" id="" className="border border-black rounded-lg" />
+                </div>
             </div>
-            <div>
-                <label htmlFor="">Durée</label>
-                <input type="text" name="" id="" className="border border-black rounded-lg" />
+            <div className="border border-black">
+                <p>Réalisation</p>
+                <div>
+                    <label htmlFor="">IA scénario</label>
+                    <input type="text" name="" id="" className="border border-black rounded-lg" />
+                </div>
+                <div>
+                    <label htmlFor="">IA générative vidéo</label>
+                    <input type="text" name="" id="" className="border border-black rounded-lg" />
+                </div>
+                <div>
+                    <label htmlFor="">IA sons et musiques</label>
+                    <input type="text" name="" id="" className="border border-black rounded-lg" />
+                </div>
+                <div>
+                    <label htmlFor="">IA post-production</label>
+                    <input type="text" name="" id="" className="border border-black rounded-lg" />
+                </div>
             </div>
-            <div>
-                <label htmlFor="">Image</label>
-                <input type="file" name="" id="" className="border border-black rounded-lg" />
+            <div className="border border-black">
+                <p>Informations complémentaires</p>
+                <div>
+                    <label htmlFor="">Producteur</label>
+                    <input type="text" name="" id="" className="border border-black rounded-lg" />
+                </div>
+                <div>
+                    <label htmlFor="">Instagram</label>
+                    <input type="text" name="" id="" className="border border-black rounded-lg" />
+                </div>
+                <div>
+                    <label htmlFor="">Linkedin</label>
+                    <input type="text" name="" id="" className="border border-black rounded-lg" />
+                </div>
+                <div>
+                    <label htmlFor="">Youtube</label>
+                    <input type="text" name="" id="" className="border border-black rounded-lg" />
+                </div>
             </div>
-            <div>
-                <label htmlFor="">IA scénario</label>
-                <input type="text" name="" id="" className="border border-black rounded-lg" />
-            </div>
-            <div>
-                <label htmlFor="">IA générative vidéo</label>
-                <input type="text" name="" id="" className="border border-black rounded-lg" />
-            </div>
-            <div>
-                <label htmlFor="">IA sons et musiques</label>
-                <input type="text" name="" id="" className="border border-black rounded-lg" />
-            </div>
-            <div>
-                <label htmlFor="">IA post-production</label>
-                <input type="text" name="" id="" className="border border-black rounded-lg" />
-            </div>
-            <div>
-                <label htmlFor="">Producteur</label>
-                <input type="text" name="" id="" className="border border-black rounded-lg" />
-            </div>
-            <div>
-                <label htmlFor="">Instagram</label>
-                <input type="text" name="" id="" className="border border-black rounded-lg" />
-            </div>
-            <div>
-                <label htmlFor="">Linkedin</label>
-                <input type="text" name="" id="" className="border border-black rounded-lg" />
-            </div>
-            <div>
-                <label htmlFor="">Youtube</label>
-                <input type="text" name="" id="" className="border border-black rounded-lg" />
-            </div>
-            <button type="submit">Valider</button>
+            <button type="submit" className="bg-amber-300 p-2 rounded-lg">Finaliser ma soumission</button>
         </form>
     )
 }
