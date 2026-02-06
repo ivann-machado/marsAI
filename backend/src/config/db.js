f8ce2abe8474009b15fcf023a624148ce7539799
Ivann <ivann.machado@laplateforme.io>
2026-02-06 08:47:12 +0100

    ind

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,11 +6,11 @@ import mariadb from "mariadb";
  * @type {import('mariadb').Pool}
  */
 const pool = mariadb.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  connectionLimit: 5,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	connectionLimit: 5,
 });
 
 /**
@@ -18,15 +18,15 @@ const pool = mariadb.createPool({
  * @returns {Promise<void>}
  */
 const connectDB = async () => {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    console.log("Connected to the database");
-  } catch (err) {
-    console.error("Error connecting to the database: ", err);
-  } finally {
-    if (conn) conn.release();
-  }
+	let conn;
+	try {
+		conn = await pool.getConnection();
+		console.log("Connected to the database");
+	} catch (err) {
+		console.error("Error connecting to the database: ", err);
+	} finally {
+		if (conn) conn.release();
+	}
 };
 
 export { pool };
