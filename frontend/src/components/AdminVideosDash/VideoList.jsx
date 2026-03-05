97ac5fc4330666db8a3e37043b05804cff305f48
cosmin-bilga <cosmin.bilga@laplateforme.io>
2026-03-05 14:49:56 +0100

    videos menu wip

--- frontend/src/components/AdminVideosDash/VideoList.jsx
+++ frontend/src/components/AdminVideosDash/VideoList.jsx
@@ -1,35 +1,46 @@
 function VideoCard({ video, type }) {
+  const selectVideo = () => {};
+
   return (
-    <div
-      key={video.id}
-      className="grid grid-cols-6 w-full mx-4 my-2 text-center border-t p-1"
-    >
-      <img src={video.cover_image} className="m-auto"></img>
+    <div className="grid grid-cols-6 w-full mx-4 my-2 text-center border-t p-1">
+      <img src={video.cover_image} className="m-auto rounded-md "></img>
       <p className="flex justify-center items-center">{video.title}</p>
       <p className="flex justify-center items-center">{video.producer}</p>
-      <p
-        className={
-          (video.status === "unverified"
-            ? "bg-amber-500"
-            : video.status === "verified" || video.status === "selected"
-              ? "bg-green-600"
-              : "bg-red-700") + " flex justify-center items-center"
-        }
-      >
-        {video.status}
-      </p>
-      <a
-        href={"/video/" + video.id}
-        className="flex justify-center items-center"
-      >
-        {type === "queue" ? "Noter Film" : "Details"}
-      </a>
-      <div
-        className={
-          "w-8 h-4 bg-gray-600 m-auto " +
-          (video.status === "selected" ? " bg-green-600 " : " bg-red-700 ")
-        }
-      ></div>
+      <div className="flex justify-center items-center">
+        <p
+          className={
+            (video.status === "unverified"
+              ? "bg-amber-500"
+              : video.status === "verified" || video.status === "selected"
+                ? "bg-green-600"
+                : "bg-red-700") + " p-2 rounded-2xl"
+          }
+        >
+          {video.status}
+        </p>
+      </div>
+      <div className="flex justify-center items-center ">
+        <a
+          href={"/video/" + video.id}
+          className="bg-gray-700 p-2 rounded-md hover:ring-2 hover:ring-purple-600"
+        >
+          {type === "queue" ? "Noter Film" : "Details"}
+        </a>
+      </div>
+      {type != "queue" ? (
+        <div
+          className={
+            "w-8 h-4 bg-gray-600 m-auto " +
+            (video.status === "selected" ? " bg-green-600 " : " bg-red-700 ")
+          }
+        >
+          <p>
+            className=
+            {"w-8 h-4 bg-gray-600 m-auto " +
+              (video.status === "selected" ? " bg-green-600 " : " bg-red-700 ")}
+          </p>
+        </div>
+      ) : null}
     </div>
   );
 }
@@ -63,7 +74,7 @@ function VideoList(props) {
 
       {props.videoList.map((video) =>
         checkFilter(filters, video) ? (
-          <VideoCard video={video} type={props.type} />
+          <VideoCard video={video} key={video.id} type={props.type} />
         ) : null,
       )}
     </div>
