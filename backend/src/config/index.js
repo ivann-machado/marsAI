96699fa85614b89710b197d98582a073baf65663
Ivann <ivann.machado@laplateforme.io>
2026-03-11 14:28:32 +0100

    moved helmet config here

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -60,3 +60,10 @@ export const CORS_OPTIONS = {
 	credentials: true,
 	methods: ["GET", "POST", "PUT", "DELETE"]
 };
+
+export const HELMET_CONFIG = {
+	contentSecurityPolicy: false,
+	crossOriginEmbedderPolicy: false,
+	crossOriginOpenerPolicy: false,
+	crossOriginResourcePolicy: false,
+};
