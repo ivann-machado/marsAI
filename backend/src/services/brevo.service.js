422bd1ef5ec0430b3861b1d621a29036b601ef01
Ivann <ivann.machado@laplateforme.io>
2026-03-13 16:46:13 +0100

    import reflect brevo.js rename

--- backend/src/services/brevo.service.js
+++ backend/src/services/brevo.service.js
@@ -1,4 +1,4 @@
-import { getBrevoClient } from '../config/brevo.js';
+import { BrevoClient } from '../config/brevo.config.js';
 import { DEV_MODE } from '../config/index.js';
 
 /**
@@ -44,14 +44,13 @@ export const sendEmail = async (to, subject, htmlContent, textContent = '', send
 			console.log(`Preparing to send email to: ${to}`);
 		}
 
-		const { api, config } = getBrevoClient();
-		const sendSmtpEmail = configureBaseEmail(to, config, senderOverride);
+		const sendSmtpEmail = configureBaseEmail(to, BrevoClient.config, senderOverride);
 
 		sendSmtpEmail.subject = subject;
 		sendSmtpEmail.htmlContent = htmlContent;
 		sendSmtpEmail.textContent = textContent || htmlContent.replace(/<[^>]*>/g, '');
 
-		const response = await api.sendTransacEmail(sendSmtpEmail);
+		const response = await BrevoClient.api.sendTransacEmail(sendSmtpEmail);
 
 		const messageId = response.messageId || response.body?.messageId;
 		if (DEV_MODE) {
@@ -81,13 +80,12 @@ export const sendTemplateEmail = async (to, templateId, params = {}) => {
 			console.log(`Preparing to send template email (${templateId}) to: ${to}`);
 		}
 
-		const { api, config } = getBrevoClient();
-		const sendSmtpEmail = configureBaseEmail(to, config);
+		const sendSmtpEmail = configureBaseEmail(to, BrevoClient.config);
 
 		sendSmtpEmail.templateId = templateId;
 		sendSmtpEmail.params = params;
 
-		const response = await api.sendTransacEmail(sendSmtpEmail);
+		const response = await BrevoClient.api.sendTransacEmail(sendSmtpEmail);
 
 		const messageId = response.body?.messageId || response.messageId;
 		if (DEV_MODE) {
