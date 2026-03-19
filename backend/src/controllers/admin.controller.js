7147301977cc0e7ee7f483f0e004b38917a36f86
Rsoumre <harounasoumare547@gmail.com>
2026-03-19 08:37:19 +0100

    Revert "delete linkyoutoube"
    This reverts commit aa248032c8cf11dd7bf661ff21092aa2b9eb5922.

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -133,7 +133,18 @@ export const getOverview = async (req, res) => {
 			prisma.reservations.count(),
 			prisma.newsletters.count(),
 			prisma.prized_videos.count(),
-			prisma.videos.findMany
+			prisma.videos.findMany({
+				where: {
+					youtube_link: {
+						not: "",
+					},
+				},
+				select: {
+					id: true,
+					title: true,
+					youtube_link: true,
+				},
+			}),
 		]);
 
 		// Build video status breakdown
