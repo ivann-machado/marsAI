c7b050926f998470a8722a87411903e63fdd4723
Ivann <ivann.machado@laplateforme.io>
2026-03-24 12:18:08 +0100

    connect and error log

--- backend/src/config/redis.config.js
+++ backend/src/config/redis.config.js
@@ -1,3 +1,13 @@
 import Redis from 'ioredis';
 
-export default new Redis(process.env.REDIS_URL);
+const redis = new Redis(process.env.REDIS_URL);
+
+redis.on('connect', () => {
+	console.log('Redis connected');
+});
+
+redis.on('error', (err) => {
+	console.error('Redis error:', err);
+});
+
+export default redis;
