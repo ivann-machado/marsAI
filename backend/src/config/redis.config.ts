bf9f421c1c0d250a85a131c764f8b5cd7b24fcbe
Ivann <ivann.machado@laplateforme.io>
2026-03-24 15:01:35 +0100

    changed to typescript

--- backend/src/config/redis.config.ts
+++ backend/src/config/redis.config.ts
@@ -1,12 +1,16 @@
 import Redis from 'ioredis';
 
-const redis = new Redis(process.env.REDIS_URL);
+if (!process.env.REDIS_URL) {
+	throw new Error('REDIS_URL environment variable is missing');
+}
+
+const redis = new Redis(process.env.REDIS_URL as string);
 
 redis.on('connect', () => {
 	console.log('Redis connected');
 });
 
-redis.on('error', (err) => {
+redis.on('error', (err: Error) => {
 	console.error('Redis error:', err);
 });
 
