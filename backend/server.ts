00b777b4da9321f1e73d18cfb05ad20ead3dad5b
Ivann <ivann.machado@laplateforme.io>
2026-03-24 15:00:55 +0100

    refactored server.ts to be cleaner aswell as the import

--- backend/server.ts
+++ backend/server.ts
@@ -1,78 +1,26 @@
-import { performance } from 'perf_hooks';
-import prisma from './src/config/prisma.config.js';
+import { performance as perf } from 'node:perf_hooks';
+import { createServer, type Server, type RequestListener } from 'node:http';
 import { loadSettings } from './src/config/settings.js';
-import { createServer } from 'http';
-import type { Server } from 'http';
-import type { RequestListener } from 'http';
+import { startServer, gracefulShutdown } from './src/utils/server.util.ts';
 import app from './src/app.js';
 
 const PORT: number = Number(process.env.PORT) || 3000;
 
-console.log(`Starting server in ${process.env.NODE_ENV} mode at ${Math.round(Date.now() - performance.timeOrigin)}ms process time.`);
-console.log(`Process ID: ${process.pid}`);
+console.log(`Starting server in ${process.env.NODE_ENV} mode at ${Math.round(Date.now() - perf.timeOrigin)}ms process time.\nProcess ID: ${process.pid}`);
 
 const settingStartTime = Date.now();
+
 await loadSettings();
-console.log(`Settings loaded in ${Date.now() - settingStartTime}ms at ${Math.round(Date.now() - performance.timeOrigin)}ms process time`);
+console.log(`Settings loaded in ${Date.now() - settingStartTime}ms at ${Math.round(Date.now() - perf.timeOrigin)}ms process time`);
 
 const server: Server = createServer(app as RequestListener);
 
-server.on('listening', () => {
-	console.log(`Server started on http://localhost:${PORT} in ${Math.round(Date.now() - performance.timeOrigin)}ms process time.`);
-});
+server.on('listening', () => console.log(`Server started on http://localhost:${PORT} in ${Math.round(Date.now() - perf.timeOrigin)}ms process time.`));
 
-const startServer = (port: number, retries: number = 20): void => {
-	server.removeAllListeners('error');
-	server.on('error', (err: NodeJS.ErrnoException) => {
-		if (err.code === 'EADDRINUSE') {
-			if (retries > 0) {
-				console.log(`Port ${port} is in use, retrying in 1 second... (${retries} retries left)`);
-				setTimeout(() => {
-					server.close();
-					startServer(port, retries - 1);
-				}, 1000);
-			} else {
-				console.error(`Port ${port} is still busy after multiple attempts. Exiting.`);
-				process.exit(1);
-			}
-		} else {
-			throw err;
-		}
-	});
-	server.listen(port);
-};
+startServer(server, PORT);
 
-startServer(PORT);
-
-const gracefulShutdown = (err?: Error | any): void => {
-	const isSignal = typeof err === 'string' && err.startsWith('SIG');
-	if (err instanceof Error) {
-		console.error('Uncaught Exception or Rejection :', err);
-	}
-	else {
-		console.log(`Received kill signal ${isSignal ? `(${err}) ` : ''}shutting down gracefully`);
-	}
-	const forceExit = setTimeout(() => {
-		console.error('Could not close connections in time, forcefully shutting down');
-		process.exit(1);
-	}, 10000);
-
-	server.close(async () => {
-		console.log('Closed out remaining connections');
-		try {
-			await prisma.$disconnect();
-			console.log('Database disconnected');
-			clearTimeout(forceExit);
-			process.exit((!isSignal && err) ? 1 : 0);
-		} catch (dbErr) {
-			console.error('Error disconnecting database', dbErr);
-			process.exit(1);
-		}
-	});
-};
-
-process.on('SIGTERM', gracefulShutdown);
-process.on('SIGINT', gracefulShutdown);
-process.on('SIGUSR2', gracefulShutdown);
-process.on('uncaughtException', gracefulShutdown);
-process.on('unhandledRejection', gracefulShutdown);
+process.on('SIGTERM', (signal: string) => gracefulShutdown(server, signal))
+	.on('SIGINT', (signal: string) => gracefulShutdown(server, signal))
+	.on('SIGUSR2', (signal: string) => gracefulShutdown(server, signal))
+	.on('uncaughtException', (err: Error) => gracefulShutdown(server, err))
+	.on('unhandledRejection', (err: Error) => gracefulShutdown(server, err));
