5036f64ea5535df90fa287041a4d9d704e9226c4
pierre-canet <pierre.canet@laplateforme.io>
2026-03-31 16:21:31 +0200

    optimisations mineures

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,24 +3,23 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		react({
-			babel: {
-				plugins: [["babel-plugin-react-compiler"]],
-			},
-		}),
-	],
-	build: {
-		assetsInlineLimit: 0,
-		sourcemap: 'hidden',
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					if (id.includes("node_modules")) {
-						return "vendor";
-					}
-				},
-			},
-		},
-	}
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
+  build: {
+    assetsInlineLimit: 0,
+    sourcemap: "hidden",
+    rollupOptions: {
+      treeshake: {
+        moduleSideEffects: false,
+      },
+      output: {
+        preserveModules: true,
+      },
+    },
+  },
 });
