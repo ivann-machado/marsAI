d8aea10baa0bd32db8102e7d4d97b0faed58ae10
Ivann <ivann.machado@laplateforme.io>
2026-04-02 10:08:57 +0200

    ts migr, alias pathing import and ratelimiter moved to its own cfg file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,29 +1,27 @@
-import express from "express";
+import express, { type Request, type Response, type NextFunction } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import rateLimit from "express-rate-limit";
-import debounce from "./middlewares/debounce.middleware.js";
-import { JWT_SECRET } from "./config/index.ts";
-import CORS_OPTIONS from "./config/cors.config.ts";
-import jwt from "jsonwebtoken";
-import MORGAN_FORMAT from "./config/morgan.config.ts";
-import HELMET_CONFIG from "./config/helmet.config.ts";
+import { debounce } from "#middlewares";
+import { CORS_OPTIONS, MORGAN_FORMAT, HELMET_CONFIG, LIMITER_CONFIG } from "#config";
+
 // Routes imports
-import authRoutes from "./routes/auth.routes.js";
-import settingRoutes from "./routes/setting.routes.js";
-import contentRoutes from "./routes/content.routes.js";
-import videoRoutes from "./routes/videos.routes.js";
-import newsletterRoutes from "./routes/newsletter.routes.js";
-import reservationRoutes from "./routes/reservation.routes.js";
-import eventRoutes from "./routes/event.routes.js";
-import contactRoutes from "./routes/contact.routes.js";
-import juryRoutes from "./routes/jury.routes.js";
-import sponsorRoutes from "./routes/sponsor.routes.js";
-import reviewRoutes from "./routes/review.routes.js";
-import prizedRoutes from "./routes/prized.routes.js";
-import highlightRoutes from "./routes/highlight.routes.js";
-import adminRoutes from "./routes/admin.routes.js";
+import {
+	authRoutes,
+	settingRoutes,
+	contentRoutes,
+	videoRoutes,
+	newsletterRoutes,
+	reservationRoutes,
+	eventRoutes,
+	contactRoutes,
+	juryRoutes,
+	sponsorRoutes,
+	reviewRoutes,
+	prizedRoutes,
+	highlightRoutes,
+	adminRoutes
+} from "#routes";
 
 const app = express();
 app.set("trust proxy", Number(process.env.PROXY_TRUST) || 0);
@@ -41,30 +39,10 @@ app.use(morgan(MORGAN_FORMAT)); // Log requests
 app.use(debounce(500));
 
 // Rate Limiting
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per windowMs
-	handler: (req, res, next, options) => {
-		res.status(options.statusCode).send(
-			`Too many requests from this IP, please try again after ${options.windowMs / (60 * 1000)} minutes`,
-		);
-	},
-	skip: (req) => {
-		try {
-			const token = req.headers["authorization"]?.split(" ")[1];
-			if (!token) return false;
-
-			const decoded = jwt.verify(token, JWT_SECRET);
-			return decoded.role === "admin" || decoded.role === "super_admin";
-		} catch (error) {
-			return false;
-		}
-	},
-});
-app.use(limiter);
+app.use(LIMITER_CONFIG);
 
 // Public routes
-app.get("/", (_req, res) => {
+app.get("/", (_req: Request, res: Response) => {
 	res.status(200).json({ message: "Welcome to MarsAI API" });
 });
 app.use("/api/auth", authRoutes);
@@ -86,12 +64,12 @@ app.use("/api/content", contentRoutes);
 // API Documentation
 if (process.env.NODE_ENV !== "production") {
 	const { default: swaggerUi } = await import("swagger-ui-express");
-	const { default: swaggerSpec } = await import("./config/swagger.config.ts");
+	const { default: swaggerSpec } = await import("#config/swagger.config");
 	app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
 
 // Global Error Handling Middleware
-app.use((err, _req, res, _next) => {
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
 	console.error(err.stack);
 	res.status(500).json({
 		status: "error",
