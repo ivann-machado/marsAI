e2e662cedab9a46ccac157d976580ccd80d36077
Ivann <ivann.machado@laplateforme.io>
2026-04-02 05:34:30 +0200

    method chaining

--- backend/src/config/redis.config.ts
+++ backend/src/config/redis.config.ts
@@ -6,12 +6,12 @@ if (!process.env.REDIS_URL) {
 
 const redis = new Redis(process.env.REDIS_URL as string, { maxRetriesPerRequest: null });
 
-redis.on('connect', () => {
-	console.log('Redis connected');
-});
-
-redis.on('error', (err: Error) => {
-	console.error('Redis error:', err);
-});
+redis
+	.on('connect', () => {
+		console.log('Redis connected');
+	})
+	.on('error', (err: Error) => {
+		console.error('Redis error:', err);
+	});
 
 export default redis;
